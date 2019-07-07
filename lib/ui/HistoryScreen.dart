feab43a7eec55fa42cc8b265358f7974e5a3bb70
emvaized <3.touch.apps@gmail.com>
2019-07-08 02:06:15 +0300

    Little bug fixes and improvements.

--- lib/ui/HistoryScreen.dart
+++ lib/ui/HistoryScreen.dart
@@ -14,7 +14,7 @@ class _HistoryScreen2State extends State<HistoryScreen> with TickerProviderState
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async {
-        Navigator.pop(context, {'newLastTimersList': widget.lastTimers});
+        Navigator.pop(context, {'newLastTimersList': widget.lastTimers.reversed.toList()});
         return false;
       },
       child: Hero(
@@ -71,89 +71,82 @@ class _HistoryScreen2State extends State<HistoryScreen> with TickerProviderState
                     child: AnimatedList(
                         initialItemCount: widget.lastTimers.length,
                         itemBuilder: (_, int position, animation) {
-                          return FadeTransition(
-                            opacity:
-                                animation.drive(Tween(begin: 0.0, end: 1.0)),
-                            child: Dismissible(
-                                key: UniqueKey(),
-                                onDismissed: (direction) {
-                                  widget.lastTimers.removeAt(position);
-                                  setState(() {});
-                                  AnimatedList.of(_).removeItem(
-                                      position, (context, animation) => Card());
-                                },
-                                child: position == 0
-                                    ? buildFirstCard(position, context)
-                                    : buildCard(position, context)),
-                          );
+                          return Dismissible(
+                              key: UniqueKey(),
+                              onDismissed: (direction) {
+                                widget.lastTimers.removeAt(position);
+                                setState(() {});
+                                AnimatedList.of(_).removeItem(
+                                    position, (context, animation) => Container());
+
+                              },
+                              child: Padding(
+                                padding: EdgeInsets.all(12.0),
+                                  child: position == 0
+                                  ? buildFirstCard(position, context)
+                                  : buildCard(position, context)));
                         }))),
       ),
     );
   }
 
   buildCard(int position, BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: Card(
-        color: Colors.white,
-        elevation: 6,
-        child: ListTile(
-            title: Padding(
-                padding: EdgeInsets.all(5),
-                child: Text(
-                  widget.lastTimers[position],
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 25),
-                )),
-            trailing: Icon(Icons.restore),
-            leading: Container(width: 30),
-            onTap: () {
-              Navigator.pop(context, {
-                'timer': Duration(
-                    hours: int.parse(
-                        widget.lastTimers[position].toString().split(':')[0]),
-                    minutes: int.parse(
-                        widget.lastTimers[position].toString().split(':')[1]),
-                    seconds: int.parse(
-                        widget.lastTimers[position].toString().split(':')[2]))
-              });
-            }),
-      ),
+    return Card(
+      color: Colors.white,
+      elevation: 6,
+      child: ListTile(
+          title: Padding(
+              padding: EdgeInsets.all(5),
+              child: Text(
+                widget.lastTimers[position],
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25),
+              )),
+          trailing: Icon(Icons.restore),
+          leading: Container(width: 30),
+          onTap: () {
+            Navigator.pop(context, {
+              'timer': Duration(
+                  hours: int.parse(
+                      widget.lastTimers[position].toString().split(':')[0]),
+                  minutes: int.parse(
+                      widget.lastTimers[position].toString().split(':')[1]),
+                  seconds: int.parse(
+                      widget.lastTimers[position].toString().split(':')[2]))
+            });
+          }),
     );
   }
 
   buildFirstCard(int position, BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: Card(
-        color: Colors.white,
-        elevation: 6,
-        child: ListTile(
-            title: Padding(
-                padding: EdgeInsets.only(bottom: 1, top: 5),
-                child: Text(
-                  widget.lastTimers[position],
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 25),
-                )),
-            subtitle: Text(
-              '(last timer)',
-              textAlign: TextAlign.center,
-            ),
-            trailing: Icon(Icons.restore),
-            leading: Container(width: 30),
-            onTap: () {
-              Navigator.pop(context, {
-                'timer': Duration(
-                    hours: int.parse(
-                        widget.lastTimers[position].toString().split(':')[0]),
-                    minutes: int.parse(
-                        widget.lastTimers[position].toString().split(':')[1]),
-                    seconds: int.parse(
-                        widget.lastTimers[position].toString().split(':')[2]))
-              });
-            }),
-      ),
+    return Card(
+      color: Colors.white,
+      elevation: 6,
+      child: ListTile(
+          title: Padding(
+              padding: EdgeInsets.only(bottom: 1, top: 5),
+              child: Text(
+                widget.lastTimers[position],
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25),
+              )),
+          subtitle: Text(
+            '(last timer)',
+            textAlign: TextAlign.center,
+          ),
+          trailing: Icon(Icons.restore),
+          leading: Container(width: 30),
+          onTap: () {
+            Navigator.pop(context, {
+              'timer': Duration(
+                  hours: int.parse(
+                      widget.lastTimers[position].toString().split(':')[0]),
+                  minutes: int.parse(
+                      widget.lastTimers[position].toString().split(':')[1]),
+                  seconds: int.parse(
+                      widget.lastTimers[position].toString().split(':')[2]))
+            });
+          }),
     );
   }
 }
